ec7f78063e7a04d99ae161c3c906d896ef1dc881
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-21 13:17:26 -0500

    Tests and code finalized

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -5,33 +5,36 @@ Last Modified: 06/21/2020
 
 Program to sort and search arrays.
 """
-import array as ar
+import array as arr
 
 '''Function to accept user input.
 :user_input: input from user.
 :returns: User input'''
 def get_input():
-    user_input = input('Please enter a number from 1 to 9.\n')
+    user_input = input('Please enter a number from 0 to 9.\n')
     return user_input
 
 '''Function to accept user input and verify it is a positive whole number from 1 to 9.
 :input: User input passed into the function.
 :valid_input: Input that has been shows to be an integer
 :returns: valid_input'''
-def validate_input(input):
+def validate_input(user_input):
     while True:
         try:
-            valid_input = int(input)
+            valid_input = int(user_input)
             if valid_input > 9:
-                print('Number too high. Choose a number from 1 to 9')
+                print('Number too high. Choose a number from 0 to 9')
+                user_input = input()
                 continue
-            elif valid_input < 1:
-                print('Number too low. Choose a number from 1 to 9')
+            elif valid_input < 0:
+                print('Number too low. Choose a number from 0 to 9')
+                user_input = input()
                 continue
             else:
                 break
         except ValueError:
-            print('Invalid input. Please use only numbers from 1 to 9')
+            print('Invalid input. Please use only numbers from 0 to 9')
+            user_input = input()
             continue
     return valid_input
 
@@ -41,20 +44,22 @@ loop to populate it. Results are returned.
 :array_length: The final length desired of the array.
 :returns: Populated array'''
 def build_array():
-    output_array = []
+    input_list = []
     array_length = 3
 
     for index in range(array_length):
-        output_array.append(validate_input(get_input()))
+        input_list.append(validate_input(get_input()))
 
+    output_array = arr.array('i', input_list)
     return output_array
 
-'''This returns a sorted version of the ANIMAL_LIST.
-:returns: sorted ANIMAL_LIST'''
-def sort_list():
-    return sorted(ANIMAL_LIST)
-    '''This function uses "return" as it uses the sorted() func which doesn't sort the 
-    original list, instead returning a different, modified version. Without the return here 
+'''This returns a sorted version of an input array.
+:returns: sorted input_array'''
+def sort_array(input_array):
+    sorted_array = arr.array('i', sorted(arr.array.tolist(input_array)))
+    return sorted_array
+    '''This function uses "return" as it uses the sorted() func which doesn't modify the 
+    original array, instead returning a new sorted instance. Without the return here 
     you would have to add the print func into the function, making it do double duty. It also 
     lets you pass the returned list into other code so it aids in modularity.'''
 
@@ -66,3 +71,8 @@ def search_array(searched_array, search_item):
         return searched_array.index(search_item)
     except ValueError:
         return -1
+
+if __name__ == '__main__':
+    new_array = build_array()
+    print(search_array(new_array, 3))
+    print(sort_array(new_array))
--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -15,23 +15,23 @@ class Array_Test_Case(unittest.TestCase):
 
     '''Test to make sure build_array makes the expected array.'''
     @patch('fun_with_collections.sort_and_search_array.get_input', return_value='7')
-    def test_build_array(self):
-        self.assertEqual(SSarray.build_array(), [7, 7, 7])
+    def test_build_array(self, input):
+        self.assertEqual(SSarray.build_array(), arr.array('i', [7, 7, 7]))
 
-    '''Test to make sure search_list returns correct index position.'''
+    '''Test to make sure search_array returns correct index position.'''
     def test_search_found(self):
-        test_array = arr.array('i', [1, 3, 5, 6, 7, 9, 0, 2, 4, 6, 8])
-        self.assertEqual(SSarray.search_array(test_array, 7), 4)
+        test_array = arr.array('i', [1, 3, 5, 7, 9, 0, 2, 4, 6, 8])
+        self.assertEqual(SSarray.search_array(test_array, 7), 3)
 
-    '''Test to make sure search_list returns -1 if item not present in list.'''
+    '''Test to make sure search_array returns -1 if item not present in array.'''
     def test_search_not_found(self):
-        test_array = arr.array('i', [1, 3, 5, 6, 7, 9, 0, 2, 4, 6, 8])
+        test_array = arr.array('i', [1, 3, 5, 7, 9, 0, 2, 4, 6, 8])
         self.assertEqual(SSarray.search_array(test_array, 'pangolin'), -1)
 
-    '''Test to make sure sort_list returns correctly sorted list.'''
+    '''Test to make sure sort_list returns correctly sorted array.'''
     def test_sort_array(self):
-        test_array = arr.array('i', [1, 3, 5, 6, 7, 9, 0, 2, 4, 6, 8])
-        self.assertEqual(SSarray.sort_array(test_array), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        test_array = arr.array('i', [1, 3, 5, 7, 9, 0, 2, 4, 6, 8])
+        self.assertEqual(SSarray.sort_array(test_array), arr.array('i', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
 
 
